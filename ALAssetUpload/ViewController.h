a6ae698ba1532a18d8f1218ac50ee36704ff160a
Dmitry Sochnev <Groove@MacBook-Pro-Dmitry.local>
2015-03-31 14:01:43 +0300

    Initial Commit

---
+++ ALAssetUpload/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ALAssetUpload
+//
+//  Created by Dmitry Sochnev on 31.03.15.
+//  Copyright (c) 2015 Dmitry Sochnev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
